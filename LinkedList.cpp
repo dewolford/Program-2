cbab4c0f358df06ee72ff3dfa53402e23f89c7e3
Dani Wolford <dewolford42@tntech.edu>
2024-10-21 14:48:03 -0500

    Dani LinkedList edits

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,3 +2,54 @@
     Title: classes.cpp
     Purpose: hold all functions necessary for the classes made in classes.h
 */
+#include "LinkedList.h"
+#include "SmartPointer.h"
+#include "Restaurants.h"
+
+//constructor
+
+//destructor
+
+//setters
+void setHeadPtr(int* h)
+{
+    
+}
+
+void setTailPtr(int* t)
+{
+
+}
+
+void setSize(int s)
+{
+
+}
+
+
+
+//getters
+int* getHeadPtr()
+{
+    return headPtr;
+}
+
+int* getTailPtr()
+{
+    return tailPtr;
+}
+
+int getSize()
+{
+    return size;
+}
+
+//add to list (append, prepend, insert)
+
+//get from list (back, front, at)
+
+//remove from list (pop, remove, etc)
+
+//sorting function
+
+//stream opperator (<<)
--- LinkedList.h
+++ LinkedList.h
@@ -6,7 +6,46 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-#include "LinkedList.h"
+#include "SmartPointer.h"
 #include "Restaurants.h"
+#include <iostream>
+#include <list>
+
+class LinkedList
+{
+    private:
+        int* headPtr;
+        int* tailPtr;
+        int size;
+
+    public:
+        //constructors
+        LinkedList();
+        LinkedList(int*, int*, int);
+        
+        //destructor
+        LinkedList();
+        
+        //setters
+        void setHeadPtr(int*);
+        void setTailPtr(int*);
+        void setSize(int);
+
+        //getters
+        int* getHeadPtr();
+        int* getTailPtr();
+        int getSize();
+        
+        //add to list (append, prepend, insert)
+        
+        //get from list (back, front, at)
+        
+        //remove from list (pop, remove, etc)
+        
+        //sorting function
+        
+        //stream opperator (<<)
+
+};
 
 #endif
