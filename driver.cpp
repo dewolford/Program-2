d1c9d1b1e1f6d361fcfd1048832f2d5a57ff38ba
Gabriel Simpkins <gcsimpkins42@gmail.com>
2024-10-22 10:25:33 -0500

    added Driver case statement and general format (Gabe)

--- driver.cpp
+++ driver.cpp
@@ -2,3 +2,63 @@
     Title: driver.cpp
     Purpose: hold the main function
 */
+#include "LinkedList.h"     //
+#include "SmartPointer.h"   //
+#include "Restaurants.h"    //
+#include "functions.cpp"    //
+
+using namespace std;
+
+int main(){
+
+    int choice; //contains the users choice for the menu
+    bool cont = true; //used in the do while loop to determine if they quit or not
+
+    
+    do {
+        //prints the main menu
+        printMenu();                /*why is this erroring? can someone fix if its still here? thanks -Gabe*/
+        //takes and validates input 
+        validateInput(choice);      //this is borrowed from project 1
+
+        switch(choice){
+
+            case 1:
+                // add a restaurant case
+
+                /*call constrctor for restaurant?*/
+                /*call append list funtion*/
+
+                break;
+            case 2: 
+                //delete a restaurant case
+
+                /*call destructor for restraunt*/
+                /*call remove list function*/
+                break;
+            case 3: 
+                //display a restaurant case
+
+                /*call << Restaurant function*/
+                break;
+            case 4:
+                //sort restaurant list case
+
+                /*AHHHHHHHHHHH (call sort linked list function? maybe smart pointer idk)*/
+                break;
+            case 5: 
+                //Compare restaurants case
+                
+                /*call restaurant < or > or = idk yet ill figure it out -Gabe*/
+                break;
+            default:
+                //this case quits if any number besides 1-5 are entered
+                cout << "\n\nYou have successfully quit! Thanks for visiting!";
+                cont = false;
+                break;
+        }
+    } while(cont);//continues if anything but quit is chosen
+
+    return 0;//end of code
+
+}/* its giving me an error here but i think its trippin, if not, my Bad-Gabe*/
--- functions.cpp
+++ functions.cpp
@@ -2,3 +2,26 @@
     Title: functions.cpp
     Purpose: hold the extra functions 
 */
+#include <iostream>
+
+using namespace std;
+
+void printMenu();
+int validateInput(int);
+
+                            /*is this formatting right? -Gabe*/
+
+void printMenu(){
+    cout << "MAIN MENU (Enter a Number):\n\t(1) Add a Restaurant\n\n\t(2) Delete a Restaurant\n\n\t(3) Display a Restaurant\n\n\t(4) Sort Restaurants\n\n\t(5) Compare Restaurants\n\n\t(6) Quit\n\n";
+}
+int validateInput(int num)
+{
+    while(!(cin >> num))
+    {
+        cout << "Please enter a valid number" << endl;
+        cin.clear(); //clears all stream bits (good, bad, eof, fail)
+        cin.ignore(10000, '\n'); //removes up to 10000 characters from buffer until a newline is removed
+        cout << "\nEnter a number: ";
+    }
+    return num;
+}
